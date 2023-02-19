855432a309b4d1f1ca9627bfcd4201aa0c59966e
Ibrahim Okić <ibrahimokic711@gmail.com>
2023-02-19 18:49:05 +0100

    Posts, Stories & Comment UI

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
+import Posts from "../../components/posts/Posts"
+import Stories from "../../components/stories/Stories"
 import "./home.scss"
 
 function Home() {
     return (
-        <div className="home">Home</div>
+        <div className="home">
+            <Stories />
+            <Posts />
+        </div>
     )
 }
 
