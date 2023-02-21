dd11d43d6b8f1031b806be1f741ed36ea6591586
Ibrahim Okić <ibrahimokic711@gmail.com>
2023-02-21 19:29:47 +0100

    Adding new post

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,28 +1,44 @@
 import { db } from "../db.js";
 import jwt from "jsonwebtoken"
+import moment from "moment/moment.js";
 
 export const getPosts = (req, res) => {
 
-    const userId = req.query.userId;
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).json('Not logged in!');
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+        if (err) return res.status(403).json('Token is not valid');
+        const getPostsQuery = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+        LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
+        ORDER BY p.createdAt DESC`;
+
+        db.query(getPostsQuery, [userInfo.id, userInfo.id], (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data);
+        })
+    })
+
+}
+export const addPost = (req, res) => {
+
     const token = req.cookies.accessToken;
     if (!token) return res.status(401).json('Not logged in!');
 
     jwt.verify(token, "secretkey", (err, userInfo) => {
         if (err) return res.status(403).json('Token is not valid');
 
-        const getPostsQuery =
-            userId !== "undefined"
-                ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
-                : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
-                LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
-                ORDER BY p.createdAt DESC`;
+        const insertNewPost = "INSERT INTO posts (`description`, `img`, `createdAt`, `userId`) VALUES (?)";
+        const values = [
+            req.body.description,
+            req.body.img,
+            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            userInfo.id
+        ]
 
-        const values =
-            userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
-
-        db.query(getPostsQuery, [values], (err, data) => {
+        db.query(insertNewPost, [values], (err, data) => {
             if (err) return res.status(500).json(err);
-            return res.status(200).json(data);
+            return res.status(200).json('Post has been added');
         })
     })
 
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getPosts } from "../controllers/post.js";
+import { getPosts, addPost } from "../controllers/post.js";
 
 const router = express.Router();
 
 router.get('/', getPosts)
+router.post('/add', addPost)
 
 export default router;
